b0f39656bf604089d165c9bf027ea8bf13bcb254
Alex Prosser <apsoutherngaming07@gmail.com>
2020-12-28 19:43:21 -0600

    2015 styff

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,17 +1,37 @@
 const fs = require('fs');
+const fetch = require('node-fetch');
+const htmlParser = require('node-html-parser');
+const htmlToMarkdown = require('turndown')();
+require('dotenv').config();
+
+htmlToMarkdown.keep(['span']);
+htmlToMarkdown.addRule('header2', {
+    filter: ['h2'],
+    replacement: content => {
+        return '## ' + content.slice(1);
+    }
+});
 
 let createDay = async (day, year) => {
-    let dir = `./days/${year}/day${day}`;
-    
-    fs.mkdirSync(dir);
-    fs.writeFileSync(dir + '/part1.txt');
-    fs.writeFileSync(dir + '/part2.txt');
+    const response = await fetch(
+        `https://adventofcode.com/${year}/day/${day}`, { headers: { cookie: `session=${process.env.SESSION_ID}` } }
+    );
+
+    const html = await response.text();
+    const root = htmlParser.parse(html);
+    const markdown = htmlToMarkdown.turndown(root.querySelectorAll('.day-desc')[0].innerHTML + '\n' + root.querySelectorAll('.day-desc')[1].innerHTML);
+
+    let dir = `./years/${year}/day${day}`;
+    if (!fs.existsSync(dir)) fs.mkdirSync(dir);
+
+    fs.writeFile(dir + '/README.md', markdown, (err) => {
+        if (err) throw err;
+        console.log(`Saved the README of day ${day}, year ${year}!`);
+    });
 }
 
 (async () => {
-    for (let year = 2015; year <= 2020; year++) {
-        for (let day = 1; day <= 25; day++) {
-            await createDay(day, year);
-        }
+    for (let day = 1; day <= 25; day++) {
+        await createDay(day, 2015);
     }
 })();
