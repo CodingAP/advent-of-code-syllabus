da944bb2a447291526666a4adfb8615a2517c602
CodingAP <apsoutherngaming07@gmail.com>
2020-12-30 15:42:47 -0600

    fixing some issues

--- scripts/generator.js
+++ scripts/generator.js
@@ -1,37 +1,29 @@
 const fs = require('fs');
-const fetch = require('node-fetch');
-const htmlParser = require('node-html-parser');
-const htmlToMarkdown = require('turndown')();
-require('dotenv').config();
-
-htmlToMarkdown.keep(['span']);
-htmlToMarkdown.addRule('header2', {
-    filter: ['h2'],
-    replacement: content => {
-        return '## ' + content.slice(1);
-    }
-});
 
 let createDay = async (day, year) => {
-    const response = await fetch(
-        `https://adventofcode.com/${year}/day/${day}`, { headers: { cookie: `session=${process.env.SESSION_ID}` } }
-    );
-
-    const html = await response.text();
-    const root = htmlParser.parse(html);
-    const markdown = htmlToMarkdown.turndown(root.querySelectorAll('.day-desc')[0].innerHTML + '\n' + root.querySelectorAll('.day-desc')[1].innerHTML);
-
     let dir = `./years/${year}/day${day}`;
     if (!fs.existsSync(dir)) fs.mkdirSync(dir);
 
-    fs.writeFile(dir + '/README.md', markdown, (err) => {
+    fs.writeFile(dir + '/README.md', '## Nothing Yet', (err) => {
+        if (err) throw err;
+        console.log(`Saved the README of day ${day}, year ${year}!`);
+    });
+
+    fs.writeFile(dir + '/part1.txt', '', (err) => {
+        if (err) throw err;
+        console.log(`Saved the README of day ${day}, year ${year}!`);
+    });
+
+    fs.writeFile(dir + '/part2.txt', '', (err) => {
         if (err) throw err;
         console.log(`Saved the README of day ${day}, year ${year}!`);
     });
 }
 
 (async () => {
-    for (let day = 1; day <= 25; day++) {
-        await createDay(day, 2015);
+    for (let year = 2015; year <= 2020; year++) {
+        for (let day = 1; day <= 25; day++) {
+            await createDay(day, year);
+        }
     }
 })();
